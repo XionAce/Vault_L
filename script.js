af8052454dc8cab1b06b34282d497068d3b33925
XionAce <xionacemahigugmaon041@gmail.com>
2025-02-12 20:44:06 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let enteredPassword = "";
+const correctPassword = "071023"; // Change this to your desired passcode
+const redirectURL = "https://xionace.github.io/For_abiii/"; // Change to your destination link
+
+function addNumber(num) {
+    if (enteredPassword.length < 6) {
+        enteredPassword += num;
+        document.getElementById("password").value = "•".repeat(enteredPassword.length);
+    }
+}
+
+function clearPassword() {
+    enteredPassword = "";
+    document.getElementById("password").value = "";
+}
+
+function checkPassword() {
+    if (enteredPassword === correctPassword) {
+        window.location.href = redirectURL;
+    } else {
+        alert("Incorrect password! Try again.");
+        clearPassword();
+    }
+}
